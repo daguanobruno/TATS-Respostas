6c95d473893861e120336fa996504208dba6409c
Bruno Daguano <bruno_daguano@hotmail.com>
2018-08-22 00:08:27 -0300

    Atividade 04

---
+++ TATS-Respostas/Atividade4/test/CalcularSalarioTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CalcularSalarioTest {
+    
+    public CalcularSalarioTest() {
+    }
+    
+    @Test
+    public void TestDesenvolvedor(){
+        CalcularSalario c = new CalcularSalario();
+        Funcionario f = new Funcionario();
+        
+        f.setCargo("DESENVOLVEDOR");
+        f.setSalarioBase(3000);
+        
+        c.calcular(f.getCargo(), f.getSalarioBase());
+        
+        assertEquals("Calculo Aceito", c.MensagemErro);
+    }
+
+ 
+}
